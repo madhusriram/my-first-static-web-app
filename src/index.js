34ffa5926db9519c8305ffa18c862e473925ca51
Madhu Sriramu <madhusriramu@microsoft.com>
2023-01-26 15:06:38 -0800

    drastic change

--- src/index.js
+++ src/index.js
@@ -1,7 +1,98 @@
+import { render } from '@testing-library/react/dist/pure';
 import React from 'react';
 import ReactDOM from 'react-dom';
+import React from 'react';
 import './index.css';
-import App from './App';
-import Results from './Results';
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      fields: {},
+      errors: {}
+    }
+  }
+/*
+  const handleSubmit = event => {
+    event.preventDefault();
+    const formData = new FormData(event.target);
+    const data = [...formData.entries()];
+
+    // TODO validate v4/v6 address
+    const qryString = data
+    .map(x => `${encodeURIComponent(x[0])}=${encodeURIComponent(x[1])}`)
+    .join('&');
+    
+    // validate address
+
+    console.log("/api/mapReq?" + qryString);
+
+    return fetch("/api/mapReq?" + qryString)
+    .then(response => response.text())
+    .then(data => console.log(data));
+
+    // const qryString = new URLSearchParams(formData).toString();
+    // as an improvement, implement caching of URI to serve previously returned answers
+    // maybe set a timer to purge the cache regularly
+  }
+
+  return (
+    <div className="wrapper">
+    <h1>ATM Mapper</h1>
+    <form id="nameform" onSubmit={handleSubmit}>
+      <fieldset>
+        <legend> Enter IP Address and a routing method!</legend>
+        <label for="ip">IP:</label>
+        <input type="text" id="ip" name="ip" required></input>
+        <label for="routingmethod-select">Routing Method: </label>
+        <select name="routingMethod" id="routingmethod-select">
+          <option value="">--Please choose an option--</option>
+          <option value="geo">Geo</option>
+          <option value="default">Default(Perf)</option>
+        </select>
+      </fieldset>
+      <button type="submit" class="buttonClass">Submit</button>
+    </form>
+    </div>
+  )
+*/
+
+  handleSubmit(e){
+    e.preventDefault();
+    if (!this.handleValidation()) {
+      alert("Form has errors!")
+    }
+  }
+
+  handleChange(field, e){
+    let fields = this.state.fields;
+    fields[field] = e.target.value;
+    this.setState({fields});
+  }
+
+  render() {
+    return (
+    <div className="wrapper">
+      <h1>ATM Mapper</h1>
+      <form id="nameform" className="nameform" onSubmit= {this.contactSubmit.bind(this)}>
+        <fieldset>
+          <legend> Enter IP Address and a routing method!</legend>
+          <input ref="ip" type="text" size="30" onChange={this.handleChange.bind(this, "ip")} value={this.state.fields["ip"]}/>
+          <span className="error">{this.state.errors["name"]}</span>
+          <select name="routingMethod" id="routingMethod-select">
+            <option value="">--Please choose an option</option>
+            <option value="geo">Geo</option>
+            <option value="default">Default(Perf)</option>
+          </select>
+        </fieldset>
+        <button type="submit" class="buttonClass">Submit</button>
+      </form>
+    </div>
+    )
+    }
+}
+
+//export default App;
 
 ReactDOM.render(<App />, document.getElementById('root'));
