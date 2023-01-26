53aac86e2247de65b4307820668fd1d446240672
Madhu Sriramu <madhusriramu@microsoft.com>
2023-01-26 15:46:09 -0800

    drastic change

--- src/index.js
+++ src/index.js
@@ -114,7 +114,7 @@ class App extends React.Component {
         <fieldset>
           <legend> Enter IP Address and a routing method!</legend>
           <input ref="ip" type="text" size="30" onChange={this.handleChange.bind(this, "ip")} value={this.state.fields["ip"]}/>
-          <span className="error">{this.state.errors["name"]}</span>
+          <span className="error">{this.state.errors["ip"]}</span>
           <select name="routingMethod" id="routingMethod-select">
             <option value="">--Please choose an option</option>
             <option value="geo">Geo</option>
