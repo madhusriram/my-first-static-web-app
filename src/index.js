02e9313cbc31d5555a1311741914586bba73f897
Madhu Sriramu <madhusriramu@microsoft.com>
2023-01-28 13:56:24 -0800

    pass data between clases

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { isIPV4Address, isIPV6Address } from 'ip-address-validator';
 class Result extends React.Component {
   // do any formatting needed here!
   render() {
-
+    console.log("About to be render now: " + this.props.result);
     return this.props.result;
   }
 }
